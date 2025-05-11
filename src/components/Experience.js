bdfffe6c389d92f8bd804a6e3844377ee220c0b4
Alyssa Meczkowska <alyssa.meczkowska@gmail.com>
2025-05-11 01:03:34 -0400

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -19,7 +19,7 @@ export const Experience = () => {
             <div class="content">
             <h2>Vázquez Group | Mar '25 – Present</h2>
             <h3>Cheminformatics and Machine Learning Researcher</h3>
-            <p>Supports clustering analysis to identify meaningful molecular groupings, applies machine learning and large language models to cheminformatics, 
+            <p>Supports clustering analysis to identify meaningful molecular groupings, applies machine learning and large language models to cheminformatics, and
             uses Vim and the terminal for Python coding, data analysis, and workflow automation</p>
             </div>
          </div>
@@ -40,7 +40,7 @@ export const Experience = () => {
             <div class="content">
               <h2>Student Tech Governance | Oct '24 – Present</h2>
               <h3>Governance Member</h3>
-              <p>Advocates for technology integration in student services and academic programs, collaborates with faculty and administration to evaluate technology on campus</p>
+              <p>Advocates for technology integration in student services and academic programs, collaborates with faculty and administration to evaluate and improve campus technologies, and represents student perspectives in university-wide IT decision-making and planning efforts</p>
             </div>
           </div>
         </div>
@@ -51,8 +51,8 @@ export const Experience = () => {
             <div class="content">
               <h2>ULC | Oct '24 – May '25</h2>
               <h3>Computer Science and Mathematics Tutor</h3>
-              <p>Meets with students individually or in small groups to provide academic assistance, 
-                  establishes relationships with students and serve as a resource for other campus support, create social media content to guide students academically</p>
+              <p>Met with students individually or in small groups to provide academic assistance, 
+                  established relationships with students and served as a resource for other campus support, and created social media content to guide students academically</p>
             </div>
           </div>
         </div>
@@ -62,7 +62,7 @@ export const Experience = () => {
               <h2>Star Learning Center | Oct '19 – June '20</h2>
               <h3>Volunteer Geometry and Biology Tutor</h3>
               <p>Tutored high school students one-on-one in person and virtually during COVID-19, 
-                  prepared students for tests by identifying and improving development areas, 
+                  prepared students for tests by identifying and improving development areas, and 
                    created practice problems based on specific subject matter</p>
             </div>
           </div>
