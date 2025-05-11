274658041bdbd58c887252e58d026f7073a2dcf7
Alyssa Meczkowska <alyssa.meczkowska@gmail.com>
2025-05-11 00:24:20 -0400

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,44 +7,66 @@ export const Experience = () => {
         <div class="card">
           <div class="box">
             <div class="content">
-            <h2>Mar '25 – Present</h2>
+            <h2>Bukhari Lab | Apr '25 – Present</h2>
+            <h3>Machine Learning Researcher</h3>
+            <p>Extracts clinical signals from unstructured EHR text using transformer-based language models, fine-tunes BioBERT for entity recognition, 
+                integrates biomedical ontologies and knowledge graphs, and supports predictive modeling in Python with scikit-learn and spaCy.</p>
+            </div>
+           </div>
+          </div>
+        <div class="card">
+          <div class="box">
+            <div class="content">
+            <h2>Vázquez Group | Mar '25 – Present</h2>
             <h3>Cheminformatics and Machine Learning Researcher</h3>
             <p>Supports clustering analysis to identify meaningful molecular groupings, applies machine learning and large language models to cheminformatics, 
             uses Vim and the terminal for Python coding, data analysis, and workflow automation</p>
             </div>
          </div>
         </div>
-        <div class="card">
+    </div>
+    <div class="container">
+     <div class="card">
           <div class="box">
             <div class="content">
-              <h2>Oct '24 – Present</h2>
-              <h3>Computer Science and Mathematics Tutor</h3>
-              <p>Meets with students individually or in small groups to provide academic assistance, establishes relationships with students and serve as a resource for other campus support, create social media content to guide students academically</p>
+              <h2>ACM Student Chapter | May '25 – Present</h2>
+              <h3>Social Media Coordinator</h3>
+              <p>Manages digital outreach for the ACM chapter, promotes technical events, and fosters community engagement through social media</p>
             </div>
           </div>
         </div>
-      </div>
-      <div class="container">
         <div class="card">
           <div class="box">
             <div class="content">
-              <h2>Oct '24 – Present</h2>
-              <h3>Student Technology Governance Member</h3>
+              <h2>Student Technology Governance Group| Oct '24 – Present</h2>
+              <h3>Governance Member</h3>
               <p>Advocates for technology integration in student services and academic programs, collaborates with faculty and administration to evaluate technology on campus</p>
             </div>
           </div>
         </div>
+       </div>
+      <div class="container">
       <div class="card">
-    <div class="box">
-      <div class="content">
-        <h2>Oct '19 – June '20</h2>
-        <h3>Volunteer Geometry and Biology Tutor</h3>
-        <p>Tutored high school students one-on-one in person and virtually during COVID-19, 
-            prepared students for tests by identifying and improving development areas, 
-             created practice problems based on specific subject matter</p>
-      </div>
-    </div>
-      </div>
+          <div class="box">
+            <div class="content">
+              <h2>University Learning Commons | Oct '24 – May '25</h2>
+              <h3>Computer Science and Mathematics Tutor</h3>
+              <p>Meets with students individually or in small groups to provide academic assistance, 
+                  establishes relationships with students and serve as a resource for other campus support, create social media content to guide students academically</p>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="box">
+            <div class="content">
+              <h2>Star Learning Center | Oct '19 – June '20</h2>
+              <h3>Volunteer Geometry and Biology Tutor</h3>
+              <p>Tutored high school students one-on-one in person and virtually during COVID-19, 
+                  prepared students for tests by identifying and improving development areas, 
+                   created practice problems based on specific subject matter</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   )
