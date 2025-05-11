f86c9005fc0121096ae6fe2f3e6dad6119274293
Alyssa Meczkowska <alyssa.meczkowska@gmail.com>
2025-05-11 01:13:57 -0400

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -15,9 +15,9 @@ export const Banner = () => {
               <img src={self} alt="Header Img"/></div>    
               <Container>
                 <div className="spacing">
-                  <p id="spacing">I'm a third-year Computer Science student at St. John’s University with a strong academic foundation and hands-on experience in research, tutoring, and technology advocacy. 
-                    <br></br><br></br>Currently, I’m conducting cheminformatics and machine learning research, applying clustering analysis and large language models to uncover insights in molecular data. 
-                    I enjoy combining technical knowledge relating to quantum physics with scientific curiosity to contribute to meaningful research and innovation.</p>
+                  <p id="spacing">I'm a third-year Computer Science student at St. John’s University with a strong academic foundation and hands-on experience in research, technology outreach, and student advocacy.
+                  <br></br><br></br> Currently, I’m conducting research in cheminformatics, clinical NLP, and quantum computing, applying clustering methods, transformer models, and cryptographic frameworks to extract insights from scientific and medical data. 
+                  I enjoy combining technical depth with curiosity to drive innovation and build inclusive, research-driven communities.</p>
                   <div className="info">
                   <a href="Resume_AM.pdf" target="_blank" rel="noopener noreferrer" className="a a2 one"> Resume <ArrowRightCircle size={25} /></a>
                     <a href="mailto:alyssa.meczkowska@gmail.com" className="a a2 two"> Contact<ArrowRightCircle size={25} /></a>
