13920893ef48c412f63c08caff417594591855b9
Alyssa Meczkowska <alyssa.meczkowska@gmail.com>
2025-05-11 00:49:36 -0400

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -31,14 +31,14 @@ export const Experience = () => {
             <div class="content">
               <h2>ACM Student Chapter | May '25 – Present</h2>
               <h3>Social Media Coordinator</h3>
-              <p>Manages digital outreach for the ACM chapter, promotes technical events, and fosters community engagement through social media</p>
+              <p>Manages digital outreach for the ACM chapter, promotes technical events, fosters community engagement through social media, and collaborates with the executive board to support technical events, peer engagement, and community building</p>
             </div>
           </div>
         </div>
         <div class="card">
           <div class="box">
             <div class="content">
-              <h2>Student Technology Governance Group| Oct '24 – Present</h2>
+              <h2>Student Tech Governance | Oct '24 – Present</h2>
               <h3>Governance Member</h3>
               <p>Advocates for technology integration in student services and academic programs, collaborates with faculty and administration to evaluate technology on campus</p>
             </div>
@@ -49,7 +49,7 @@ export const Experience = () => {
       <div class="card">
           <div class="box">
             <div class="content">
-              <h2>University Learning Commons | Oct '24 – May '25</h2>
+              <h2>ULC | Oct '24 – May '25</h2>
               <h3>Computer Science and Mathematics Tutor</h3>
               <p>Meets with students individually or in small groups to provide academic assistance, 
                   establishes relationships with students and serve as a resource for other campus support, create social media content to guide students academically</p>
