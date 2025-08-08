4498a721aaf073264c064843afa5e85aec9dea17
Alyssa Meczkowska <alyssa.meczkowska@gmail.com>
2025-08-08 16:37:18 -0400

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,58 +4,75 @@ export const Experience = () => {
     <section className="experiences" id="experience">      
     <h2>Experience</h2>
       <div class="container">
+         <div class="card">
+            <div class="box">
+              <div class="content">
+              <h2>CCPS | May '25 – Present</h2>
+              <h3>Quantum Computing Research Assistant</h3>
+              <p>Simulating hybrid Kyber/Dilithium PQC and BB84 QKD protocols in OMNeT++ to secure high-mobility vehicular networks, benchmarking performance across latency, throughput, and cryptographic metrics</p>
+              </div>
+             </div>
+            </div>
         <div class="card">
           <div class="box">
             <div class="content">
             <h2>Bukhari Lab | Apr '25 – Present</h2>
-            <h3>Machine Learning Researcher</h3>
-            <p>Extracts clinical signals from unstructured EHR text using transformer-based language models, fine-tunes BioBERT for entity recognition, 
-                integrates biomedical ontologies and knowledge graphs, and supports predictive modeling in Python with scikit-learn and spaCy.</p>
+            <h3>Machine Learning Research Assistant</h3>
+            <p>Engineering Medicaddie, an NSF-funded clinician review platform for AI-predicted ICD/CPT codes, and fine-tuned BioBERT on 10K+ MIMIC-IV notes to detect clinician burnout with 0.84 F1</p>
             </div>
            </div>
           </div>
+        </div>
+      <div class="container">
         <div class="card">
           <div class="box">
             <div class="content">
             <h2>Vázquez Group | Mar '25 – Present</h2>
-            <h3>Cheminformatics and Machine Learning Researcher</h3>
-            <p>Supports clustering analysis to identify meaningful molecular groupings, applies machine learning and large language models to cheminformatics, and
-            uses Vim and the terminal for Python coding, data analysis, and workflow automation</p>
+            <h3>Cheminformatics Research Assistant/h3>
+            <p>Computing quantum chemistry descriptors for 1K+ molecules and trained Random Forest/XGBoost models (R² = 0.71) to predict keto–enol equilibrium constants, funded by the Clare Boothe Luce award</p>
             </div>
          </div>
         </div>
-    </div>
-    <div class="container">
      <div class="card">
           <div class="box">
             <div class="content">
               <h2>ACM Student Chapter | May '25 – Present</h2>
               <h3>Social Media Coordinator</h3>
-              <p>Manages digital outreach for the ACM chapter, promotes technical events, fosters community engagement through social media, and collaborates with the executive board to support technical events, peer engagement, and community building</p>
+              <p>Managing digital outreach for the ACM chapter, promoting technical events, fostering community engagement through social media, and collaborating with the executive board to support technical events, peer engagement, and community building</p>
             </div>
           </div>
         </div>
+    </div>
+    <div class="container">
         <div class="card">
           <div class="box">
             <div class="content">
               <h2>Student Tech Governance | Oct '24 – Present</h2>
               <h3>Governance Member</h3>
-              <p>Advocates for technology integration in student services and academic programs, collaborates with faculty and administration to evaluate and improve campus technologies, and represents student perspectives in university-wide IT decision-making and planning efforts</p>
+              <p>Advocating for technology integration in student services and academic programs, collaborates with faculty and administration to evaluate and improve campus technologies, and represents student perspectives in university-wide IT decision-making and planning efforts</p>
             </div>
           </div>
         </div>
+        <div class="card">
+              <div class="box">
+                <div class="content">
+                  <h2>Daisy Property Management | May '25 – Aug '25</h2>
+                  <h3>Operations Intern</h3>
+                  <p>Developed API-driven workflow automations processing 5K+ documents, cutting processing time by 95% and reducing manual intervention from 3 days to under 5 minutes</p>
+                </div>
+              </div>
+            </div>
        </div>
       <div class="container">
-      <div class="card">
-          <div class="box">
-            <div class="content">
-              <h2>ULC | Oct '24 – May '25</h2>
-              <h3>Computer Science and Mathematics Tutor</h3>
-              <p>Met with students individually or in small groups to provide academic assistance, 
-                  established relationships with students and served as a resource for other campus support, and created social media content to guide students academically</p>
+        <div class="card">
+            <div class="box">
+              <div class="content">
+                <h2>ULC | Oct '24 – May '25</h2>
+                <h3>Computer Science and Mathematics Tutor</h3>
+                <p>Delivered 150+ hours of tailored instruction to 30+ students, boosting average grades by 25% and earning “Tutor of the Month” for innovative problem-solving modules</p>
+              </div>
             </div>
           </div>
-        </div>
         <div class="card">
           <div class="box">
             <div class="content">
